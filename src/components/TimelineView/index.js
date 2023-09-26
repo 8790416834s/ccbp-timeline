7b6ec0d0766c6b5a3b8617504f8f743b4f8669ba
root <cheripellysravan0411@gmail.com>
2023-09-26 07:52:34 +0000

    not display

--- src/components/TimelineView/index.js
+++ src/components/TimelineView/index.js
@@ -6,48 +6,8 @@ import CourseTimelineCard from '../CourseTimelineCard'
 import './index.css'
 
 class TimelineView extends Component {
-  getProjectFilter = () => {
-    const {timelineItemsList} = this.props
-    const projectFilter = timelineItemsList.filter(each =>
-      each.categoryId === 'PROJECT' ? each : '',
-    )
-    return projectFilter
-  }
-
-  getCourseFilter = () => {
-    const {timelineItemsList} = this.props
-    const courseFilter = timelineItemsList.filter(each =>
-      each.categoryId === 'COURSE' ? each : '',
-    )
-    return courseFilter
-  }
-
-  visitBtn = projectUrl => {
-    console.log(`id ${projectUrl}`)
-  }
-
-  courseData = data => ({
-    title: data.title,
-    cardTitle: data.courseTitle,
-    cardSubtitle: data.duration,
-    cardDetailedText: data.description,
-  })
-
-  projectData = data => ({
-    title: data.title,
-    cardTitle: data.projectTitle,
-    cardSubtitle: data.duration,
-    cardDetailedText: data.description,
-    media: data.imageUrl,
-    url: data.projectUrl,
-  })
-
   render() {
-    const courseFilter = this.getCourseFilter()
-    const projectFilter = this.getProjectFilter()
-    console.log(projectFilter)
-    const course = courseFilter.map(each => this.courseData(each))
-    const project = projectFilter.map(each => this.projectData(each))
+    const {timelineItemsList} = this.props
     return (
       <div className="main-container">
         <h1>MY JOURNEY OF</h1>
@@ -55,7 +15,7 @@ class TimelineView extends Component {
         <div className="chrono-container">
           <Chrono
             mode="VERTICAL_ALTERNATING"
-            items={project}
+            items={timelineItemsList}
             theme={{
               primary: 'red',
               secondary: 'blue',
